48a3d94a4c30e84f98a1aa4eceb6e32dd4a9e134
Alexandre Bique <bique.alexandre@gmail.com>
2011-07-08 01:13:58 +0900

    read: removed logging and added comments

--- src/read.cc
+++ src/read.cc
@@ -3,13 +3,15 @@
 int tmfs_read(const char * path, char * buf, size_t nbytes, off_t offset,
               struct fuse_file_info * fi)
 {
+  // get the real path
   std::string real_path = get_real_path(path);
 
-  std::cout << " ## read " << bfs::path(path) << " -> " << real_path << std::endl;
-
+  // open the file
   int fd = open(real_path.c_str(), O_RDONLY);
   if (fd < 0)
     return -errno;
+
+  // read the data and close
   ssize_t bytes = pread(fd, buf, nbytes, offset);
   close(fd);
   if (bytes < 0)
